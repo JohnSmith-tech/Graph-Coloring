83cd457a664ae87d045cf2ab5bd91cd218fddf28
johnsmith <iea2001@mail.ru>
2022-12-16 12:48:08 +0700

    Создание интерфейса для системы rsa

---
+++ ciphers/rsa.py
@@ -0,0 +1,25 @@
+from abc import abstractmethod, ABC
+from typing import List
+
+
+class RsaSystem(ABC):
+
+    @abstractmethod
+    def generate_c_number(self, f: int) -> int:
+        raise NotImplementedError
+
+    @abstractmethod
+    def eval_d_number(self, c: int, f: int) -> int:
+        raise NotImplementedError
+
+    @abstractmethod
+    def generate_p_q(self, v: int) -> List[list]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def eval_n(self, p: list, q: list) -> list:
+        raise NotImplementedError
+
+    @abstractmethod
+    def eval_f(self, p: list, q: list):
+        raise NotImplementedError
