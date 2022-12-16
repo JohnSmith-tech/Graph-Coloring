d1280fe19434a5d0fe8a405daf599eb834a2d40e
johnsmith <iea2001@mail.ru>
2022-12-16 17:55:40 +0700

    Запуск программы

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from coloring import GraphColoring
 
 if __name__ == '__main__':
-    app = GraphColoring(fileNameGraph='./resources/data.txt',
-                        fileNameColors='./resources/vertexColor.txt')
-    app.coloring()
+    correct = "Раскраска правильная"
+    incorrect = "Алиса пыталась обмануть"
+
+    print('Граф №1: ', end='')
+    true_graph_coloring = GraphColoring(fileNameGraph='./resources/GraphTrue.txt',
+                                        fileNameColors='./resources/TrueColor.txt')
+
+    if true_graph_coloring.coloring(alpha=5) is True:
+        print(correct)
+    else:
+        print(incorrect)
+
+    print('Граф №2: ', end='')
+
+    false_graph_coloring = GraphColoring(fileNameGraph='./resources/GraphFalse.txt',
+                                         fileNameColors='./resources/FalseColor.txt')
+
+    if false_graph_coloring.coloring(alpha=5) is True:
+        print(correct)
+    else:
+        print(incorrect)
