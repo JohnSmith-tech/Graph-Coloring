24cb06e7d20f5201ce6be0881e43b5188012c9a8
johnsmith <iea2001@mail.ru>
2022-12-16 17:56:09 +0700

    Раскраска графа

--- coloring.py
+++ coloring.py
@@ -1,6 +1,6 @@
 from methods_for_crypro.methods import *
 import random
-from sympy import isprime, randprime
+from sympy import isprime
 from typing import List
 from file import *
 from functools import reduce
@@ -26,9 +26,9 @@ class RsaGraphColoring(RsaSystem):
             q = 0
             while p == q:
                 while not isprime(p):
-                    p = randprime(2, 2**24)
+                    p = random.getrandbits(1024)
                 while not isprime(q):
-                    q = randprime(2, 2**24)
+                    q = random.getrandbits(1024)
 
             p_array.append(p)
             q_array.append(q)
@@ -37,7 +37,7 @@ class RsaGraphColoring(RsaSystem):
     def eval_n(self, p: list, q: list) -> list:
         n = []
         for i in range(len(p)):
-            n.append(p[i] + q[i])
+            n.append(p[i] * q[i])
         return n
 
     def eval_f(self, p: list, q: list):
@@ -57,23 +57,36 @@ class GraphColoring(RsaGraphColoring):
         self.__fileColor.read_file()
         self._encode_colors = {'R': '00', 'B': '01', 'Y': '10'}
 
-    def coloring(self):
-        self.p_array, self.q_array = self.generate_p_q(self.__fileGraph.v)
-        self.n = self.eval_n(self.p_array, self.q_array)
-        self.f = self.eval_f(self.p_array, self.q_array)
+    def coloring(self, alpha) -> bool:
+        for _ in range(alpha * self.__fileGraph.e):
+            self.p_array, self.q_array = self.generate_p_q(self.__fileGraph.v)
+            self.n = self.eval_n(self.p_array, self.q_array)
+            self.f = self.eval_f(self.p_array, self.q_array)
 
-        self.keys_c = [self.generate_c_number(self.f[i])
-                       for i in range(self.__fileGraph.v)]
-        self.keys_d = [self.eval_d_number(self.keys_c[i], self.f[i])
-                       for i in range(self.__fileGraph.v)]
+            self.__keys_c = [self.generate_c_number(self.f[i])
+                           for i in range(self.__fileGraph.v)]
+            self.keys_d = [self.eval_d_number(self.__keys_c[i], self.f[i])
+                           for i in range(self.__fileGraph.v)]
 
-        self.__permutation()
+            self.__permutation()
 
-        r = [int(str(random.randint(2, 2**24)) + str(self._encode_colors[self.__fileColor._colors[key]]))
-             for key in self.__fileColor._colors]
+            self.r = [int(str(random.getrandbits(512)) + str(self._encode_colors[self.__fileColor._colors[key]]))
+                      for key in self.__fileColor._colors]
 
-        z = [exp_mod(r[i], self.keys_d[i], self.n[i])
-             for i in range(self.__fileGraph.v)]
+            self.z = [exp_mod(self.r[i], self.keys_d[i], self.n[i])
+                      for i in range(self.__fileGraph.v)]
+
+            bob = Bob(edges=self.__fileGraph.edges,
+                      n=self.n, keys_d=self.keys_d, z=self.z)
+            random_edge = bob._random_egde()
+
+            c_key1, c_key2 = self.__get_two_key_c(random_edge)
+            if bob._check(c_key1=c_key1, c_key2=c_key2) is False:
+                return False
+        return True
+
+    def __get_two_key_c(self, edge) -> List[list]:
+        return [self.__keys_c[int(edge[0])-1], self.__keys_c[int(edge[1])-1]]
 
     def __get_unique_colors(self) -> list:
         unique = []
@@ -98,3 +111,31 @@ class GraphColoring(RsaGraphColoring):
             self.__fileColor._colors[key] = dict_for_perm[self.__fileColor._colors[key]]
 
 
+class Bob:
+    def __init__(self, edges, n, keys_d, z) -> None:
+        self.edges = edges
+        self.n = n
+        self.d = keys_d
+        self.z = z
+        self.curr_edge = None
+
+    def _random_egde(self) -> list:
+        index_random_edge = random.randint(
+            0, len(self.edges)-1)
+        self.curr_edge = self.edges[index_random_edge]
+        return self.edges[index_random_edge]
+
+    def __get_z_two_vertex(self, edge) -> List[list]:
+        return [self.z[int(edge[0])-1], self.z[int(edge[1])-1]]
+
+    def __get_n_two_vertex(self, edge) -> List[list]:
+        return [self.n[int(edge[0])-1], self.n[int(edge[1])-1]]
+
+    def _check(self, c_key1: int, c_key2: int) -> bool:
+        z1, z2 = self.__get_z_two_vertex(self.curr_edge)
+        n1, n2 = self.__get_n_two_vertex(self.curr_edge)
+        a = exp_mod(z1, c_key1, n1)
+        b = exp_mod(z2, c_key2, n2)
+        if str(a)[-2:] == str(b)[-2:]:
+            return False
+        return True
